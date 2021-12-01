284033827aaa4a2c02932b4ef890b2f1f48bfe8a
Albert Andersson <andersson.albert@gmail.com>
2021-12-01 10:56:00 +0100

    First skeleton

---
+++ src/main/java/se/iths/projektarbete_komplex_java/ProjektarbeteApplication.java
@@ -0,0 +1,13 @@
+package se.iths.projektarbete_komplex_java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektarbeteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektarbeteApplication.class, args);
+    }
+
+}
---
+++ src/test/java/se/iths/projektarbete_komplex_java/ProjektarbeteApplicationTests.java
@@ -0,0 +1,13 @@
+package se.iths.projektarbete_komplex_java;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjektarbeteApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
