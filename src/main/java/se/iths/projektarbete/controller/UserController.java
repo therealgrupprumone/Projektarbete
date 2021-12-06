9359a4b001903c3e2c74d8d71257237add30d9bd
JannisMueller <jannis.mueller@hotmail.de>
2021-12-06 09:43:58 +0100

    wip

--- src/main/java/se/iths/projektarbete/controller/UserController.java
+++ src/main/java/se/iths/projektarbete/controller/UserController.java
@@ -3,12 +3,15 @@ package se.iths.projektarbete.controller;
 import lombok.AllArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import se.iths.projektarbete.entity.RoleEntity;
 import se.iths.projektarbete.entity.UserEntity;
 import se.iths.projektarbete.service.RoleService;
 import se.iths.projektarbete.service.UserService;
+
+import java.util.Optional;
 
 @RestController
 @AllArgsConstructor
@@ -26,6 +29,12 @@ public class UserController {
     @GetMapping("roles")
     public ResponseEntity<Iterable<RoleEntity>> getAllRoles() {
         return ResponseEntity.ok(roleService.findAll());
+    }
+
+    @GetMapping("{id}")
+    public ResponseEntity<Optional<UserEntity>> getUserById(@PathVariable Long id){
+        return ResponseEntity.ok(userService.findById(id));
+
     }
 
 }
