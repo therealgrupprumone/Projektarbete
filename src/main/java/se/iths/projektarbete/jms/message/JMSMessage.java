b50721a22eb6e504fa7e1b98b34067cdf0a72f72
JannisMueller <jannis.mueller@hotmail.de>
2021-12-07 07:54:14 +0100

    wip

--- src/main/java/se/iths/projektarbete/jms/message/JMSMessage.java
+++ src/main/java/se/iths/projektarbete/jms/message/JMSMessage.java
@@ -1,20 +1,33 @@
 package se.iths.projektarbete.jms.message;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class JMSMessage implements Serializable {
-    private String jmsMessage;
+
+    private UUID id;
+    private String message;
+    @JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+    private LocalDateTime localDateTime;
+
 
     @Override
     public String toString() {
         return "JMSMessage{" +
-                "jmsMessage='" + jmsMessage + '\'' +
+                "id=" + id +
+                ", message='" + message + '\'' +
+                ", localDateTime=" + localDateTime +
                 '}';
     }
 }
--- src/main/java/se/iths/projektarbete/jms/message/sender/JMSSender.java
+++ src/main/java/se/iths/projektarbete/jms/message/sender/JMSSender.java
@@ -5,21 +5,24 @@ import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import se.iths.projektarbete.jms.message.JMSMessage;
 
+import java.time.LocalDateTime;
+import java.util.UUID;
+
 @Component
 public class JMSSender {
 
-    JmsTemplate jmsTemplate;
-
-    public JMSSender(JmsTemplate jmsTemplate) {
-        this.jmsTemplate = jmsTemplate;
-    }
-
-    @Scheduled(fixedRate = 2000) //every two seconds
-    public void sendMessage(){
-        System.out.println("Sending Message");
-        JMSMessage message = new JMSMessage("hej");
-        jmsTemplate.convertAndSend("MY_QUEUE",message);
-        System.out.println("Message sent!");
-
-    }
+//    JmsTemplate jmsTemplate;
+//
+//    public JMSSender(JmsTemplate jmsTemplate) {
+//        this.jmsTemplate = jmsTemplate;
+//    }
+//
+//    @Scheduled(fixedRate = 2000) //every two seconds
+//    public void sendMessage(){
+//        System.out.println("Sending Message");
+//        JMSMessage message = new JMSMessage(UUID.randomUUID(),"Hello from JU19_QUEUE!", LocalDateTime.now());
+//        jmsTemplate.convertAndSend("MY_QUEUE",message);
+//        System.out.println("Message sent!");
+//
+//    }
 }
