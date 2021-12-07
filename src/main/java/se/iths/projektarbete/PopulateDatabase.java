8afaebc45e69b3f3ec85ec171a35539b3e04997c
boalbert <andersson.albert@gmail.com>
2021-12-07 11:04:56 +0100

    Städat upp insert av data

--- src/main/java/se/iths/projektarbete/PopulateDatabase.java
+++ src/main/java/se/iths/projektarbete/PopulateDatabase.java
@@ -5,12 +5,12 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import se.iths.projektarbete.entity.FeedEntity;
+import se.iths.projektarbete.entity.MessageEntity;
 import se.iths.projektarbete.entity.RoleEntity;
 import se.iths.projektarbete.entity.UserEntity;
 import se.iths.projektarbete.repo.FeedRepo;
-import se.iths.projektarbete.repo.MessageRepo;
 import se.iths.projektarbete.repo.RoleRepo;
-import se.iths.projektarbete.repo.UserRepo;
 import se.iths.projektarbete.service.UserService;
 
 @Configuration
@@ -22,49 +22,32 @@ public class PopulateDatabase {
 
     // CommandLineRunner loadDatabase(MovieRepo movieRepo)
     @Bean
-    CommandLineRunner loadDatabase(UserRepo userRepo, RoleRepo roleRepo, MessageRepo messageRepo, FeedRepo feedRepo, UserService userService) {
+    CommandLineRunner loadDatabase(RoleRepo roleRepo, FeedRepo feedRepo, UserService userService) {
         return args -> {
 
-            RoleEntity roleEntity = new RoleEntity("ROLE_USER");
-            roleRepo.save(roleEntity);
+            var feed = new FeedEntity();
+            feedRepo.save(feed);
 
-            UserEntity userEntity = new UserEntity("Jannis", "tyskland");
-            userService.createUser(userEntity);
-//
-//            log.info("Running with profile: " + profile);
-//
-//            FeedEntity feed = new FeedEntity(List.of());
-//            feedRepo.save(feed);
-//
-////            RoleEntity admin = new RoleEntity("ROLE_ADMIN");
-//            RoleEntity roleUser = new RoleEntity("ROLE_USER");
-//
-//            roleRepo.save(roleUser);
-//
-//            UserEntity jannis = new UserEntity("jannis", "tyskland");
-//            UserEntity joakim = new UserEntity("joakim", "sverige");
-//            UserEntity albert = new UserEntity("albert", "danmark");
-//            UserEntity casper = new UserEntity("casper", "ungern");
-////            userRepo.saveAll(List.of(jannis, joakim, albert, casper));
-//
-////            jannis.addRole(admin);
-//            jannis.addRole(roleUser);
-//            joakim.addRole(roleUser);
-//            albert.addRole(roleUser);
-//            casper.addRole(roleUser);
-//
-//
-//            MessageEntity message1 = new MessageEntity("Hello from jannis", jannis, feed);
-//            MessageEntity message2 = new MessageEntity("Hello from joakim", joakim, feed);
-//
-//            jannis.addMessage(message1);
-//
-//            joakim.addMessage(message2);
-//
-//            userService.createUser(jannis);
-//            userService.createUser(joakim);
-//            userService.createUser(albert);
-//            userService.createUser(casper);
+            var roleUser = new RoleEntity("ROLE_USER");
+            var roleAdmin = new RoleEntity("ROLE_ADMIN");
+
+            roleRepo.save(roleUser);
+            roleRepo.save(roleAdmin);
+
+            var userJannis = new UserEntity("Jannis", "tyskland");
+            var userAlbert = new UserEntity("Albert", "sverige");
+
+            var msgJannis = new MessageEntity("First Hello!", userJannis, feed);
+            userJannis.addMessage(msgJannis);
+
+            var msgAlbert = new MessageEntity("Hello again!", userAlbert, feed);
+            userAlbert.addMessage(msgAlbert);
+
+            // Krypterar lösenord, assignar roll och sparar entitet via userRepo i databasen
+            userService.createUser(userAlbert, "ROLE_ADMIN");
+            userService.createUser(userJannis, "ROLE_USER");
+
+            log.info("Running with profile: " + profile);
         };
     }
 }
