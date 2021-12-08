23abf843cc74cde689707dafbae6f8fc4691ad20
JannisMueller <jannis.mueller@hotmail.de>
2021-12-08 19:29:44 +0100

    working state...

--- src/main/java/se/iths/projektarbete/jms/receiver/Receiver.java
+++ src/main/java/se/iths/projektarbete/jms/receiver/Receiver.java
@@ -10,10 +10,10 @@ import se.iths.projektarbete.jms.model.MessageObject;
 @Component
 public class Receiver {
 
-//    @JmsListener(destination = JmsConfig.CHAT_APP_QUEUE)
-//    public void listen(@Payload MessageObject messageObject) {
-//        System.out.println("I got a list with active user");
-//        System.out.println(messageObject);
-//    }
+    @JmsListener(destination = JmsConfig.CHAT_APP_QUEUE)
+    public void listen(@Payload MessageObject messageObject) {
+        System.out.println("I got a list with active user");
+        System.out.println(messageObject);
+    }
 
 }
