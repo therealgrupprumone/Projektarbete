a078944c70ff6ce760b865cddbbc46a869d6064f
Chokealot <70509435+Chokealot@users.noreply.github.com>
2021-12-13 12:15:22 +0100

    fixed message.setUsername to match with principal

--- src/main/java/se/iths/projektarbete/thymeleaf/ChatController.java
+++ src/main/java/se/iths/projektarbete/thymeleaf/ChatController.java
@@ -5,13 +5,18 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import se.iths.projektarbete.dto.Message;
+import se.iths.projektarbete.entity.UserEntity;
+import se.iths.projektarbete.repo.UserRepo;
 import se.iths.projektarbete.service.MessageService;
+
+import java.security.Principal;
 
 @Controller
 @AllArgsConstructor
 public class ChatController {
 
     MessageService messageService;
+    UserRepo userRepo;
 
     @GetMapping("/chat")
     String getChat(Model model) {
@@ -21,8 +26,8 @@ public class ChatController {
     }
 
     @RequestMapping(value = "/sendMessage", method = RequestMethod.POST)
-    String sendMessage(@ModelAttribute Message message) {
-        message.setUsername("boalbert");
+    String sendMessage(@ModelAttribute Message message, Principal principal) {
+        message.setUsername(principal.getName());
         message.setFeedId(1L);
         messageService.postMessage(message);
         return "redirect:/chat";
